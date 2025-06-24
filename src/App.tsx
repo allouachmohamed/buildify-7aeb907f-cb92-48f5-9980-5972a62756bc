d09fc7921e7f7de6425df77f5b0cec3b5ed0bb72
allouachmohamed <allouachmohamed@gmail.com>
2025-06-25 00:42:37 +0100

    UPDATE: src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import SettingsPage from './pages/SettingsPage';
 import AboutPage from './pages/AboutPage';
+import QuranPage from './pages/QuranPage';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -21,6 +22,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Layout />}>
                 <Route index element={<HomePage />} />
+                <Route path="quran" element={<QuranPage />} />
                 <Route path="settings" element={<SettingsPage />} />
                 <Route path="about" element={<AboutPage />} />
               </Route>
