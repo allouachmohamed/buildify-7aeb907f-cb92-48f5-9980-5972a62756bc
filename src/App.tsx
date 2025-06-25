719484f51bc66154e83b4470d36744e7243f9a4e
allouachmohamed <allouachmohamed@gmail.com>
2025-06-25 11:45:41 +0100

    UPDATE: src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { ThemeProvider } from './components/theme-provider';
 import { LanguageProvider } from './contexts/LanguageContext';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
+import TasbihPage from './pages/TasbihPage';
+import QuranPage from './pages/QuranPage';
+import QiblaPage from './pages/QiblaPage';
+import DhikrPage from './pages/DhikrPage';
 import SettingsPage from './pages/SettingsPage';
 import AboutPage from './pages/AboutPage';
 
@@ -21,6 +25,10 @@ function App() {
             <Routes>
               <Route path="/" element={<Layout />}>
                 <Route index element={<HomePage />} />
+                <Route path="tasbih" element={<TasbihPage />} />
+                <Route path="quran" element={<QuranPage />} />
+                <Route path="qibla" element={<QiblaPage />} />
+                <Route path="dhikr" element={<DhikrPage />} />
                 <Route path="settings" element={<SettingsPage />} />
                 <Route path="about" element={<AboutPage />} />
               </Route>
