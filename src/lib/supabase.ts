8d271c420378471552fb05bd3791776d4923bd0c
Buildify <buildify-bot@buildify.dev>
2026-04-02 13:12:03 +0000

    Delete all code

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://yprljlqqoholvrogcdfj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwcmxqbHFxb2hvbHZyb2djZGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NjIxMTgsImV4cCI6MjA5MDUzODExOH0.WCuHthcDhGdfLmXeo8P2MjnA3SJk4mdqoU9Gnv_5Efs';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
